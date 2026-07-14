1828d879f02555c68947fdf1c5ac8dffced3e503
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-07-14 12:14:13 +0530

    applicants

---
+++ frontend/src/components/admin/Applicants.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import Navbar from "../shared/Navbar";
+import ApplicantsTable from "./ApplicantsTable";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import useGetApplicants from "@/hooks/useGetApplicants";
+
+const Applicants = () => {
+    const { id } = useParams();
+    console.log("Applicants: ", id)
+
+    useGetApplicants(id);
+
+    return (
+        <div>
+            <Navbar />
+
+            <div className="max-w-7xl mx-auto my-8">
+                <h1 className="text-2xl font-bold mb-6">
+                    Applicants
+                </h1>
+
+                <ApplicantsTable />
+            </div>
+        </div>
+    );
+};
+
+export default Applicants;
