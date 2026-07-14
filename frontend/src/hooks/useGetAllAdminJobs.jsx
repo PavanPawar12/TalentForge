377aebe5da6c2b950b89ef483e825d86251e0099
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-07-14 09:32:11 +0530

    get all admin jobs

--- frontend/src/hooks/useGetAllAdminJobs.jsx
+++ frontend/src/hooks/useGetAllAdminJobs.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { JOB_API_END_POINT } from '../components/utils/constant'
+import { JOB_API_END_POINT } from '../components/utils/constant.js'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setAllAdminJobs } from '../redux/jobSlice'
-// import { setAlljobs } from '../redux/jobSlice'
 const useGetAllAdminJobs = () => {
 
     const dispatch = useDispatch();
