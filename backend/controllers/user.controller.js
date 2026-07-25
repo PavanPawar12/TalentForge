43f1be53be3539cb8b73b44871ca7bf6ecb8540e
Pavan Pawar <pavanpawar9923100@gmail.com>
2026-07-25 21:37:53 +0530

    commit changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,6 +6,8 @@ import jwt from 'jsonwebtoken'
 export const register = async (req, res) => {
     try {
         const { fullname, email, phoneNumber, password, role } = req.body;
+        console.log("filled data: ",fullname, email, phoneNumber, password, role);
+        console.log("Server Data:", req.body)
 
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
