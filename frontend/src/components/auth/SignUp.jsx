9cae31ce7109363b932e83e1eea7b89dccf937fc
Pavan Pawar <pavanpawar9923100@gmail.com>
2026-08-04 23:23:00 +0530

    commit changes

--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -78,7 +78,7 @@ const SignUp = () => {
     <div>
       <Navbar />
       <div className='flex items-center justify-center max-w-7xl mx-auto px-4 '>
-        <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
+        <form onSubmit={submitHandler} className='w-[500px] pl-12 border border-gray-200 rounded-md p-4 my-10'>
           <h1 className='font-bold text-xl mb-5'>Sign Up </h1>
           <div className='my-2'>
             <Label>Full name</Label>
@@ -117,7 +117,7 @@ const SignUp = () => {
               value={input.password}
               name="password"
               onChange={changeEventHandler}
-              placeholder='12345...'
+              placeholder=''
             />
           </div>
          
