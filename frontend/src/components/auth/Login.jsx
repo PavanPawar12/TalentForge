74ef22a355ad5964aa0c465409d8fcc0ac51dacf
Pavan Pawar <pavanpawar9923100@gmail.com>
2026-08-04 23:25:18 +0530

    commit changes

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -110,6 +110,7 @@ const Login = () => {
               </RadioGroup>
             </div>
           </div>
+          
           {
             loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin'/> Please wait </Button>:<Button type="submit" className="w-full my-4">Login</Button>
           }
